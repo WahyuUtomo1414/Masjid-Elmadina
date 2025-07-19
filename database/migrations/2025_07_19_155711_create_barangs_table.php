8685bd5da81d6c8a015cd570bb84b519aeaa5af8
WahyuTomo14 <wahyuxd14@gmail.com>
2025-07-19 23:00:00 +0700

    buat table barang

---
+++ database/migrations/2025_07_19_155711_create_barangs_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('barang', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('pengurus_id')->constrained('pengurus');
+            $table->string('nama', 128);
+            $table->integer('stok');
+            $table->enum('status_barang', ['layak', 'tidak layak']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('barang');
+    }
+};
