55db2382c13a9f8256a5bbf98bf59440a9f69276
WahyuTomo14 <wahyuxd14@gmail.com>
2025-07-21 15:34:42 +0700

    tambah widget di stats weidget

--- app/Filament/Widgets/StatsOverview.php
+++ app/Filament/Widgets/StatsOverview.php
@@ -10,7 +10,21 @@ class StatsOverview extends BaseWidget
     protected function getStats(): array
     {
         return [
-            //
+            Stat::make('Jumlah Ustadz', 4)
+                ->description('Ustadz')
+                ->descriptionIcon('heroicon-m-user-circle')
+                ->chart([2, 3, 35, 18, 15, 26, 15, 30, 25, 30, 25, 50])
+                ->color('info'),
+            Stat::make('Jumlah Pengurus', 12)
+                ->description('Pengurus')
+                ->descriptionIcon('heroicon-m-user-group')
+                ->chart([32, 23, 35, 18, 15, 56, 15, 30, 25, 30, 25, 30])
+                ->color('warning'),
+            Stat::make('Jumlah Barang', 33)
+                ->description('Barang')
+                ->descriptionIcon('heroicon-m-cube')
+                ->chart([7, 2, 10, 3, 15, 4, 17])
+                ->color('success'),
         ];
     }
 }
