8214cfb2276e7a63c8289177ca0f3ae35cf9ba70
WahyuTomo14 <wahyuxd14@gmail.com>
2025-07-21 14:31:48 +0700

    buat resource ustadz

---
+++ app/Filament/Resources/UstadzResource/Pages/EditUstadz.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Filament\Resources\UstadzResource\Pages;
+
+use App\Filament\Resources\UstadzResource;
+use Filament\Actions;
+use Filament\Resources\Pages\EditRecord;
+
+class EditUstadz extends EditRecord
+{
+    protected static string $resource = UstadzResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            Actions\DeleteAction::make(),
+            Actions\ForceDeleteAction::make(),
+            Actions\RestoreAction::make(),
+        ];
+    }
+}
